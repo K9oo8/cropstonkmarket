9138a4264a24ef863618d454e9cc552879f8d78e
K9oo8 <90291325+K9oo8@users.noreply.github.com>
2023-03-28 08:17:20 +0000

    i hope this works

--- CropMarket.py
+++ CropMarket.py
@@ -1,127 +1,152 @@
 import requests as req
 import json
-import math
-from colorama import Fore, Back, Style
+from datetime import datetime
 
-url = "https://api.hypixel.net/skyblock/bazaar"
 
-r = req.get(url).json()
+with open('index.md', 'w') as index:
+    index.write("last code update march 24th\n")
+    index.write(f"Last BZ API Call {datetime.now():%A, %B %d %H:%M:%S} UTC \n")
 
-# Harvest value is equal to 1 minute at optimal speed
-carrotHarvest = 39519
-sugarHarvest = 27074 #last update Mar/02/23
-potatoHarvest = 35840
-wheatHarvest = 12407
-melonHarvest = 58080
-pumpkinHarvest = 12180
-mushroomHarvest = 23104
-cocoaHarvest = 35260
-wartsHarvest = 31944
-cactusHarvest = 19335
+    url = "https://api.hypixel.net/skyblock/bazaar"
 
-#just defining stuff
-mushroomPerMinute = 0
-carrotsPerMinute = 0
-sugarPerMinute = 0
-potatoPerMinute = 0
-wheatPerMinute = 0
-melonperMinute = 0
-pumpkinPerMinute = 0
-cocoaPerMinute = 0
-wartsPerMinute = 0
-cactusPerMinute = 0
+    r = req.get(url).json()
+    response = req.get(url)
 
-#filtering
-mushroomInstaSell = r["products"]["ENCHANTED_RED_MUSHROOM"]['quick_status']['sellPrice']
-carrotInstaSell = r["products"]["ENCHANTED_CARROT"]['quick_status']['sellPrice']
-sugarInstaSell = r["products"]["ENCHANTED_SUGAR"]['quick_status']['sellPrice']
-potatoInstaSell = r["products"]["ENCHANTED_POTATO"]['quick_status']['sellPrice']
-wheatInstaSell = r["products"]["ENCHANTED_HAY_BLOCK"]['quick_status']['sellPrice']
-melonInstaSell = r["products"]["ENCHANTED_MELON"]['quick_status']['sellPrice']
-pumpkinInstaSell = r["products"]["ENCHANTED_PUMPKIN"]['quick_status']['sellPrice']
-cocoaInstaSell = r["products"]["ENCHANTED_COCOA"]['quick_status']['sellPrice']
-wartsInstaSell = r["products"]["ENCHANTED_NETHER_STALK"]['quick_status']['sellPrice']
-cactusInstaSell = r["products"]["ENCHANTED_CACTUS_GREEN"]['quick_status']['sellPrice']
+    if response.status_code != 200:
+        index.write("!!!API DOWN!!!")
+        exit()
 
-#more defining
-mushroomNPC = 640
-carrotNPC = 160
-sugarNPC = 320
-potatoNPC = 160
-wheatNPC = 1300
-melonNPC = 160
-pumpkinNPC = 640
-cocoaNPC = 480
-wartsNPC = 320
-cactusNPC = 160
 
-#use bigger one
-mushroom = max(mushroomInstaSell, mushroomNPC)
-carrot = max(carrotInstaSell, carrotNPC)
-sugar = max(sugarInstaSell, sugarNPC)
-potato = max(potatoInstaSell, potatoNPC)
-wheat = max(wheatInstaSell, wheatNPC)
-melon = max(melonInstaSell, melonNPC)
-pumpkin = max(pumpkinInstaSell, pumpkinNPC)
-cocoa = max(cocoaInstaSell, cocoaNPC)
-warts = max(wartsInstaSell, wartsNPC)
-cactus = max(cactusInstaSell, cactusNPC)
+    # Harvest value is equal to 1 minute at optimal speed assuming using 3/4th fermento ranchers boots
+    carrotHarvest = 39519
+    sugarHarvest = 27074 #last update Mar/02/23
+    potatoHarvest = 35840
+    wheatHarvest = 12407
+    seedHarvest = 1
+    melonHarvest = 58080
+    pumpkinHarvest = 12180
+    mushroomHarvest = 13155*2
+    cocoaHarvest = 35260
+    wartsHarvest = 31944
+    cactusHarvest = 19335
 
-#calculate how much you make inclusive of mooshroom cow pet
-def timeProfit(cropIn, cropharvest, ecraft, multiplier): #Wheat Ecraft = 1296 else 160/multiplier= 2 for cactus/sugarcane 0 for mushroom 1 for else
-    minuteout = ((cropIn*cropharvest/ecraft)+(mushroom*1200/160*multiplier))
-    return minuteout
+    #just defining stuff
+    mushroomPerHour = 0
+    carrotsPerHour = 0
+    sugarPerHour = 0
+    potatoPerHour = 0
+    wheatPerHour = 0
+    melonPerHour = 0
+    pumpkinPerHour = 0
+    cocoaPerHour = 0
+    wartsPerHour = 0
+    cactusPerHour = 0
 
-mushroomPerMinute = timeProfit(mushroom, mushroomHarvest, 160, 0)
-carrotsPerMinute = timeProfit(carrot, carrotHarvest, 160, 1)
-sugarPerMinute = timeProfit(sugar, sugarHarvest, 160, 2)
-potatoPerMinute = timeProfit(potato, potatoHarvest, 160, 1)
-wheatPerMinute = timeProfit(wheat, wheatHarvest, 1296, 1)
-melonPerMinute = timeProfit(melon, melonHarvest, 160, 1)
-pumpkinPerMinute = timeProfit(pumpkin, pumpkinHarvest, 160, 1)
-cocoaPerMinute = timeProfit(cocoa, cocoaHarvest, 160, 1)
-wartsPerMinute = timeProfit(warts, wartsHarvest, 160, 1)
-cactusPerMinute = timeProfit(cactus, cactusHarvest, 160, 2)
+    items = {
+            "ENCHANTED_RED_MUSHROOM": "mushroom",
+            "ENCHANTED_CARROT": "carrot",
+            "ENCHANTED_SUGAR": "sugar",
+            "ENCHANTED_POTATO": "potato",
+            "ENCHANTED_HAY_BLOCK": "wheat",
+            "ENCHANTED_SEEDS": "seeds",
+            "ENCHANTED_MELON": "melon",
+            "ENCHANTED_PUMPKIN": "pumpkin",
+            "ENCHANTED_COCOA": "cocoa",
+            "ENCHANTED_NETHER_STALK": "warts",
+            "ENCHANTED_CACTUS_GREEN": "cactus",
+            "FERMENTO": "fermento",
+            "SQUASH": "squash",
+            "CROPIE": "cropie"
+        }
 
-#display module
-def display(displayname, crop, cpm, cropNPC):
-    print(displayname)
-    if crop <= cropNPC:
-        print("     Sell Price:", crop, "(NPC)")
-    else:
-        print("     Sell Price:", f'{math.trunc(crop):,}')
-    #print("     Coins per Minute:", f'{math.trunc(cpm):,}')
-    print("     Coins per Hour:", f'{math.trunc(cpm*60):,}')
-    print(Style.RESET_ALL, end =" "),
+    instaSell = {}
 
-print(Fore.LIGHTMAGENTA_EX + "\x1B[6m$$$$$$$$\$$\   $$\ $$$$$$\ $$\   $$\       $$$$$$$\ $$$$$$\$$$$$$$$\ $$$$$$\ $$\   $$\$$$$$$$$\ $$$$$$\         $$$$$$\ $$$$$$$$\$$$$$$$$\       $$\      $$\ $$$$$$\ $$\   $$\$$$$$$$$\$$\     $$\ ")
-print("$$  _____$$ |  $$ $$  __$$\$$ | $$  |      $$  __$$\\\_$$ _\___$$  __$$  __$$\$$ |  $$ $$  _____$$  __$$\       $$  __$$\$$  _____\__$$  __|      $$$\    $$$ $$  __$$\$$$\  $$ $$  _____\$$\   $$  |")
-print("$$ |     $$ |  $$ $$ /  \__$$ |$$  /       $$ |  $$ | $$ |    $$ |  $$ /  \__$$ |  $$ $$ |     $$ /  \__|      $$ /  \__$$ |        $$ |         $$$$\  $$$$ $$ /  $$ $$$$\ $$ $$ |      \$$\ $$  / ")
-print("$$$$$\   $$ |  $$ $$ |     $$$$$  /        $$$$$$$\ | $$ |    $$ |  $$ |     $$$$$$$$ $$$$$\   \$$$$$$\        $$ |$$$$\$$$$$\      $$ |         $$\$$\$$ $$ $$ |  $$ $$ $$\$$ $$$$$\     \$$$$  /  ")
-print("$$  __|  $$ |  $$ $$ |     $$  $$<         $$  __$$\  $$ |    $$ |  $$ |     $$  __$$ $$  __|   \____$$\       $$ |\_$$ $$  __|     $$ |         $$ \$$$  $$ $$ |  $$ $$ \$$$$ $$  __|     \$$  /   ")
-print("$$ |     $$ |  $$ $$ |  $$\$$ |\$$\        $$ |  $$ | $$ |    $$ |  $$ |  $$\$$ |  $$ $$ |     $$\   $$ |      $$ |  $$ $$ |        $$ |         $$ |\$  /$$ $$ |  $$ $$ |\$$$ $$ |         $$ |    ")
-print("$$ |     \$$$$$$  \$$$$$$  $$ | \$$\       $$$$$$$  $$$$$$\   $$ |  \$$$$$$  $$ |  $$ $$$$$$$$\\\$$$$$$  |      \$$$$$$  $$$$$$$$\   $$ |         $$ | \_/ $$ |$$$$$$  $$ | \$$ $$$$$$$$\    $$ |    ")
-print("\__|      \______/ \______/\__|  \__|      \_______/\______|  \__|   \______/\__|  \__\________|\______/        \______/\________|  \__|         \__|     \__|\______/\__|  \__\________|   \__|    \x1B[0m")
 
-print(Fore.MAGENTA)
-display("Mushroom", mushroom, mushroomPerMinute, mushroomNPC)
-print(Fore.LIGHTRED_EX)
-display("Carrots", carrot, carrotsPerMinute, carrotNPC)
-print(Fore.LIGHTGREEN_EX)
-display("Sugar Cane", sugar, sugarPerMinute, sugarNPC)
-print(Fore.YELLOW)
-display("Potato", potato, potatoPerMinute, potatoNPC)
-print(Fore.LIGHTYELLOW_EX)
-display("Wheat", wheat, wheatPerMinute, wheatNPC)
-print(Fore.GREEN)
-display("Melon", melon, melonPerMinute, melonNPC)
-print(Fore.BLUE)
-display("Pumpkin", pumpkin, pumpkinPerMinute, pumpkinNPC)
-print(Fore.CYAN)
-display("Cocoa Beans", cocoa, cocoaPerMinute, cocoaNPC)
-print(Fore.RED)
-display("Nether Warts", warts, wartsPerMinute, wartsNPC)
-print(Fore.GREEN)
-display("Cactus", cactus, cactusPerMinute, cactusNPC)
+    for item, var_name in items.items():
+        instaSell[var_name] = r["products"][item]['quick_status']['sellPrice']
 
-input("\nPress Enter to Exit")
+
+    #insta sell
+    mushroom = instaSell["mushroom"]
+    carrot = instaSell["carrot"]
+    sugar = instaSell["sugar"]
+    potato = instaSell["potato"]
+    wheat = instaSell["wheat"]
+    seeds = instaSell["seeds"]
+    melon = instaSell["melon"]
+    pumpkin = instaSell["pumpkin"]
+    cocoa = instaSell["cocoa"]
+    warts = instaSell["warts"]
+    cactus = instaSell["cactus"]
+    fermento = instaSell["fermento"]
+    squash = instaSell["squash"]
+    cropie = instaSell["cropie"]
+
+
+    #calculate fermento squash cropie stuff per hour profit (assuming normal distribution)
+        #cropies 1/2500 or 28.8 per hour
+    cropie = cropie*28.8
+        #squash 1/5000 or 14.4 per hour
+    squash = squash*14.4
+        #fermento 1 tall 1/16,666 or 4.3 ish per hour
+    fermentosingle = fermento*4.3
+        #fermento 2 tall 1/8,333 or 8.6 ish per hour
+    fermentodouble = fermento*8.6
+
+
+    #calculate how much you make inclusive of fermento squash cropie drops
+    def timeProfit(cropIn, cropharvest, ecraft, armordrops): #Wheat Ecraft = 1296 else 160
+        hourout = ((cropIn*cropharvest/ecraft)*60 + armordrops + (cropharvest*0.2*60))
+        return hourout
+
+    #cropies
+    potatoPerHour = timeProfit(potato, potatoHarvest, 160, cropie)
+    carrotsPerHour = timeProfit(carrot, carrotHarvest, 160, cropie)
+    wheatPerHour = timeProfit(wheat, wheatHarvest, 1296, cropie)+(seeds*seedHarvest/160*60)
+
+    #squash
+    melonPerHour = timeProfit(melon, melonHarvest, 160, squash)
+    pumpkinPerHour = timeProfit(pumpkin, pumpkinHarvest, 160, squash)
+    cocoaPerHour = timeProfit(cocoa, cocoaHarvest, 160, squash)
+
+    #fermento
+    wartsPerHour = timeProfit(warts, wartsHarvest, 160, fermentosingle)
+    mushroomPerHour = timeProfit(mushroom, mushroomHarvest, 160, fermentodouble)
+
+    #doublefermento
+    sugarPerHour = timeProfit(sugar, sugarHarvest, 160, fermentodouble)
+    cactusPerHour = timeProfit(cactus, cactusHarvest, 160, fermentosingle)
+
+    #highest crop for going to blinkerton
+    crop_max = max(mushroomPerHour, carrotsPerHour, sugarPerHour, potatoPerHour, wheatPerHour, melonPerHour, pumpkinPerHour, cocoaPerHour, wartsPerHour, cactusPerHour)
+
+    
+
+    #display module
+    def display(displayname, crop, cph):
+        index.write(f'{displayname} \n')
+        index.write(f'     Sell Price: {crop:,.0f} \n')
+        index.write(f'     Coins per Hour: {cph:,.0f} \n\n')
+        
+
+    #THERE WAS A BETTER WAY TO DO THIS GOOD JOB ME
+
+    display("Mushroom", mushroom, mushroomPerHour)
+
+    display("Carrots", carrot, carrotsPerHour)
+
+    display("Sugar Cane", sugar, sugarPerHour)
+
+    display("Potato", potato, potatoPerHour)
+
+    display("Wheat (& Seeds)", wheat, wheatPerHour)
+
+    display("Melon", melon, melonPerHour)
+
+    display("Pumpkin", pumpkin, pumpkinPerHour)
+
+    display("Cocoa Beans", cocoa, cocoaPerHour)
+
+    display("Nether Warts", warts, wartsPerHour)
+
+    display("Cactus", cactus, cactusPerHour)
--- index.md
+++ index.md
@@ -1,1 +1,42 @@
-last code update march 24thMushroom       Sell Price: 1,316       Coins per Hour: 15,448,996  Carrots       Sell Price: 566       Coins per Hour: 9,587,135  Sugar Cane       Sell Price: 703       Coins per Hour: 9,614,032  Potato       Sell Price: 559       Coins per Hour: 8,663,369  Wheat (& Seeds)       Sell Price: 12,003       Coins per Hour: 7,764,003  Melon       Sell Price: 282       Coins per Hour: 7,967,179  Pumpkin       Sell Price: 1,794       Coins per Hour: 9,465,273  Cocoa Beans       Sell Price: 658       Coins per Hour: 10,248,295  Nether Warts       Sell Price: 632       Coins per Hour: 9,024,804  Cactus       Sell Price: 985       Coins per Hour: 8,446,904  
+last code update march 24th
+Last BZ API Call Tuesday, March 28 08:10:37 UTC 
+Mushroom 
+     Sell Price: 1,316 
+     Coins per Hour: 15,448,834 
+
+Carrots 
+     Sell Price: 566 
+     Coins per Hour: 9,584,167 
+
+Sugar Cane 
+     Sell Price: 704 
+     Coins per Hour: 9,619,105 
+
+Potato 
+     Sell Price: 560 
+     Coins per Hour: 8,679,912 
+
+Wheat (& Seeds) 
+     Sell Price: 12,003 
+     Coins per Hour: 7,763,899 
+
+Melon 
+     Sell Price: 283 
+     Coins per Hour: 7,976,291 
+
+Pumpkin 
+     Sell Price: 1,791 
+     Coins per Hour: 9,449,179 
+
+Cocoa Beans 
+     Sell Price: 664 
+     Coins per Hour: 10,333,284 
+
+Nether Warts 
+     Sell Price: 637 
+     Coins per Hour: 9,086,188 
+
+Cactus 
+     Sell Price: 985 
+     Coins per Hour: 8,446,495 
+
